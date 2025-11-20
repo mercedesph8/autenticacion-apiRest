3475bae3f89c2b0fa554352dc4f2871a121ff447
Mercedes <2000mariamercedes@gmail.com>
2025-11-20 23:26:25 +0100

    autenticacion.js

--- back/api/login.js
+++ back/api/login.js
@@ -1,15 +1,42 @@
-import express from "express";
-const router = express.Router();
+//Este archivo maneja el formulario de login y la comunicación con el servidor para autenticar a los usuarios
 
-//Array de usuarios que simula la base de datos
-const usuarios = [
-  {nombreUsuario: "admin", contrasenia: "admin"},
-  {nombreUsuario: "mercedes", contrasenia: "0000"},
-  {nombreUsuario: "jesus", contrasenia: "1234"},
-]
+document.addEventListener('DOMContentLoaded', () => { //Esperar a que la página se cargue completamente
 
-//Funcion para generar el tokem de autenticacion 
-function generarToken(nombreUsuario){
-  return Buffer.from(nombreUsuario).toString('base64'); //Para convertir el nombre de usuario a base64
 
+//Capturamos el formulario del login por id
+const loginForm = document.getElementById('loginForm');
+
+//Agregar un envent Listener para escuchar cuando se envie el formulario
+loginForm.addEventListener('submit', async (event) =>{
+  event.preventDefault(); //Para detener el comportamiento por defecto del formulario
+
+  //Recogemos los valores de los campos del formulario
+  const nombreUsuario = document.getElementById('nombreUsuario').value;
+  const contrasenia = document.getElementById('contrasenia').value;
+
+  //Enviamos los datos al servidor usando fetch
+  const response = await fetch ('http://localhost:3000/api/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type':'application/json'
+  },
+    body: JSON.stringify({ //Convertimos los dos valores de JS a TEXTO para que el servidor lo entienda
+      nombreUsuario: nombreUsuario,
+      contrasenia: contrasenia,
+    })
+  });
+
+//Convertimos la respuesta del servidor a JSON
+const data = await response.json();
+
+//Comprobamos si el login ha sido exitoso
+if(response.ok){
+  //Guardamos el token en el localStorage del navegador
+  localStorage.setItem('token',data.token);
+  //Redirigimos a la página de bienvenida
+  window.Location.href = '/bienvenida.html';
+} else {
+  alert("Credenciales incorrectas");
 }
+});
+});
